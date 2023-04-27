89e99b89d764a0d6a82fb2a2b19b4879d8277f79
Valts Silaputnins <valts@velns.org>
2023-04-27 16:04:19 +0300

    refactor: extract type helpers

--- src/core/sdl-docs-generator.tsx
+++ src/core/sdl-docs-generator.tsx
@@ -1,13 +1,8 @@
 import {
-  ConstListValueNode,
-  ConstValueNode,
   FieldDefinitionNode,
   GraphQLObjectType,
+  GraphQLSchema,
   Kind,
-  ListTypeNode,
-  NamedTypeNode,
-  NonNullTypeNode,
-  TypeNode,
   buildASTSchema,
   buildClientSchema,
   getIntrospectionQuery,
@@ -16,6 +11,7 @@ import {
 } from 'graphql';
 import { Maybe } from 'graphql/jsutils/Maybe';
 import { DocumentBook, DocumentPage, DocumentPageHelper } from './sdl-docs';
+import { getConstValue, getTypeName } from './sdl-type-helper';
 
 export async function getremoteSchema(url: string) {
   const { data, errors } = await fetch(url, {
@@ -40,6 +36,28 @@ export function generateBook(schema: string): DocumentBook {
   const docNode = parse(schema);
   const ast = buildASTSchema(docNode);
 
+  book['/404'] = Add404Page();
+  generateRootPage(ast, book);
+
+  const dirs = ast.getDirectives();
+  console.log(dirs);
+  const tms = ast.getTypeMap();
+  console.log(tms);
+
+  return book;
+}
+
+function Add404Page() {
+  const NotFoundPage: DocumentPage = {
+    uuid: '/404',
+    parts: [],
+  };
+  DocumentPageHelper.pushText(NotFoundPage, 'Not found');
+  DocumentPageHelper.pushLinkToPage(NotFoundPage, 'Back to start', '/');
+  return NotFoundPage;
+}
+
+function generateRootPage(ast: GraphQLSchema, book: DocumentBook) {
   const root: DocumentPage = {
     uuid: '/',
     parts: [],
@@ -57,24 +75,7 @@ export function generateBook(schema: string): DocumentBook {
   book['/subscribe'] = subscribePage;
   DocumentPageHelper.pushLinkToPage(root, 'Subscriptions', '/subscribe');
 
-  // const dirs = schema.getDirectives();
-  // const types = schema.getTypeMap();
-
-  // const print2 = printSchema(schema);
-  // console.log(print2);
-
   book['/'] = root;
-
-  const NotFoundPage: DocumentPage = {
-    uuid: '/404',
-    parts: [],
-  };
-  DocumentPageHelper.pushText(NotFoundPage, 'Not found');
-  DocumentPageHelper.pushLinkToPage(NotFoundPage, 'Back to start', '/');
-
-  book['/404'] = NotFoundPage;
-
-  return book;
 }
 
 function prepQueryPage(queryType: Maybe<GraphQLObjectType>) {
@@ -98,55 +99,6 @@ function prepQueryPage(queryType: Maybe<GraphQLObjectType>) {
     }
   }
   return page;
-}
-
-function getTypeName(type: TypeNode): [string, string?] {
-  switch (type.kind) {
-    case Kind.NAMED_TYPE:
-      const v = (type as NamedTypeNode).name.value;
-      return [v, v];
-    case Kind.LIST_TYPE:
-      const [listType, listTypeName] = getTypeName((type as ListTypeNode).type);
-      return [listType, listTypeName];
-    case Kind.NON_NULL_TYPE:
-      const [nonNullType, nonNullTypeName] = getTypeName((type as NonNullTypeNode).type);
-      return [nonNullType, nonNullTypeName];
-    default:
-      throw new Error(`Unsupported TypeNode kind: ${(type as TypeNode)?.kind}`);
-  }
-}
-
-function getConstValue(value: ConstValueNode): string {
-  switch (value.kind) {
-    case Kind.INT:
-    case Kind.FLOAT:
-    case Kind.STRING:
-    case Kind.BOOLEAN:
-    case Kind.ENUM:
-      return value.value.toString();
-    case Kind.NULL:
-      return 'NULL';
-    case Kind.LIST:
-      return (
-        '[' +
-        (value as ConstListValueNode).values
-          .map((v) => getConstValue(v))
-          .reduce((prev, curr) => prev + ', ' + curr) +
-        ']'
-      );
-    case Kind.OBJECT: {
-      let objstr = '';
-      value.fields.forEach((f) => {
-        const fieldValue = getConstValue(f.value);
-        objstr += '\n' + f.name.value + ' = ' + fieldValue;
-      });
-
-      return objstr;
-    }
-
-    default:
-      throw new Error(`Unsupported ConstValueNode kind: ${(value as ConstValueNode).kind}`);
-  }
 }
 
 function processFunction(f: FieldDefinitionNode, root: DocumentPage) {
---
+++ src/core/sdl-type-helper.ts
@@ -0,0 +1,58 @@
+import {
+  TypeNode,
+  Kind,
+  NamedTypeNode,
+  ListTypeNode,
+  NonNullTypeNode,
+  ConstValueNode,
+  ConstListValueNode,
+} from 'graphql';
+
+export function getTypeName(type: TypeNode): [string, string?] {
+  switch (type.kind) {
+    case Kind.NAMED_TYPE:
+      const v = (type as NamedTypeNode).name.value;
+      return [v, v];
+    case Kind.LIST_TYPE:
+      const [listType, listTypeName] = getTypeName((type as ListTypeNode).type);
+      return [listType, listTypeName];
+    case Kind.NON_NULL_TYPE:
+      const [nonNullType, nonNullTypeName] = getTypeName((type as NonNullTypeNode).type);
+      return [nonNullType, nonNullTypeName];
+    default:
+      throw new Error(`Unsupported TypeNode kind: ${(type as TypeNode)?.kind}`);
+  }
+}
+
+export function getConstValue(value: ConstValueNode): string {
+  switch (value.kind) {
+    case Kind.INT:
+    case Kind.FLOAT:
+    case Kind.STRING:
+    case Kind.BOOLEAN:
+    case Kind.ENUM:
+      return value.value.toString();
+    case Kind.NULL:
+      return 'NULL';
+    case Kind.LIST:
+      return (
+        '[' +
+        (value as ConstListValueNode).values
+          .map((v) => getConstValue(v))
+          .reduce((prev, curr) => prev + ', ' + curr) +
+        ']'
+      );
+    case Kind.OBJECT: {
+      let objstr = '';
+      value.fields.forEach((f) => {
+        const fieldValue = getConstValue(f.value);
+        objstr += '\n' + f.name.value + ' = ' + fieldValue;
+      });
+
+      return objstr;
+    }
+
+    default:
+      throw new Error(`Unsupported ConstValueNode kind: ${(value as ConstValueNode).kind}`);
+  }
+}
