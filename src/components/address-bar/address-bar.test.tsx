50e242e5961e51f246d0529412542f3b9dd0636f
Valts Silaputnins <valts@velns.org>
2023-04-29 23:13:32 +0300

    feat: address bar test

---
+++ src/components/address-bar/address-bar.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { AddressBar } from './address-bar';
+import { setupStore } from '@/store';
+
+describe('AddressBar component', () => {
+  const testURL = 'https://new.endpoint.com';
+
+  it('should change the endpoint when the Load button is clicked', () => {
+    const store = setupStore();
+    render(
+      <Provider store={store}>
+        <AddressBar />
+      </Provider>
+    );
+
+    const endpointInput = screen.getByRole('textbox');
+    fireEvent.change(endpointInput, { target: { value: testURL } });
+
+    const loadButton = screen.getByText('Load');
+    fireEvent.click(loadButton);
+    expect(store.getState().main.endpoint).toBe(testURL);
+  });
+
+  it('should change the endpoint when Enter key is pressed', () => {
+    const store = setupStore();
+    render(
+      <Provider store={store}>
+        <AddressBar />
+      </Provider>
+    );
+
+    const endpointInput = screen.getByRole('textbox');
+    fireEvent.change(endpointInput, { target: { value: testURL } });
+
+    fireEvent.keyDown(endpointInput, { key: 'Enter', code: 'Enter' });
+
+    expect(store.getState().main.endpoint).toBe(testURL);
+  });
+});
