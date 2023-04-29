4b1a4963679079f8462f03676998dbc26caaf64b
Valts Silaputnins <valts@velns.org>
2023-04-30 01:00:34 +0300

    feat: test schema fetching slice and thunk

--- src/core/api/api.test.tsx
+++ src/core/api/api.test.tsx
@@ -1,39 +1,11 @@
-import { SimpleMockApiClient } from '@/../__mocks__/SimpleMockApiClient';
-import { IntrospectionResponseData, getremoteSchema } from '@/core/api/api';
+import { getremoteSchema } from '@/core/api/api';
 import fuzzball from 'fuzzball';
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  graphql,
-  getIntrospectionQuery,
-  GraphQLInt,
-} from 'graphql';
+import { MOCK_QUERY_EXPECTED, setupMockIntrospection } from './api-mock-helper';
 
 describe('API test', () => {
   it('should handle schema decoding', async () => {
-    const mockSchema = new GraphQLSchema({
-      query: new GraphQLObjectType({
-        name: 'Query',
-        fields: {
-          myNumber: {
-            type: GraphQLInt,
-            resolve: () => 42, // Return any number you like
-          },
-        },
-      }),
-    });
-
-    const introspectionResult = await graphql({
-      schema: mockSchema,
-      source: getIntrospectionQuery(),
-    });
-
-    const fn = jest.fn().mockReturnValue(introspectionResult);
-    const mockClient = new SimpleMockApiClient<IntrospectionResponseData>(fn);
-
+    const mockClient = await setupMockIntrospection();
     const result = await getremoteSchema(mockClient, '://');
-    const expected = `type Query { myNumber: Int }`;
-
-    expect(fuzzball.ratio(result, expected)).toBeGreaterThan(98);
+    expect(fuzzball.ratio(result, MOCK_QUERY_EXPECTED)).toBeGreaterThan(98);
   });
 });
---
+++ src/slices/schema/schema.test.tsx
@@ -0,0 +1,65 @@
+import { AppStore, setupStore } from '@/store';
+import { waitFor } from '@testing-library/react';
+import { StoreStatus, fetchSchema } from './schema';
+import { SimpleMockApiClient } from '@/../__mocks__/SimpleMockApiClient';
+import { IntrospectionResponseData } from '@/core/api/api';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  graphql,
+  getIntrospectionQuery,
+  GraphQLInt,
+} from 'graphql';
+import { MOCK_QUERY_EXPECTED, setupMockIntrospection } from '@/core/api/api-mock-helper';
+import fuzzball from 'fuzzball';
+
+describe('schemaSlice', () => {
+  let store: AppStore;
+  const mockEndpoint = 'http://dummy.local/';
+
+  beforeEach(() => {
+    store = setupStore();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should handle fetchSchema.loading', async () => {
+    const fn = jest.fn().mockReturnValue({});
+    const mockClient = new SimpleMockApiClient<IntrospectionResponseData>(fn);
+    const initial = store.getState().schema;
+    expect(initial.status).toEqual(StoreStatus.idle);
+
+    store.dispatch(fetchSchema({ client: mockClient, endpoint: mockEndpoint }));
+    const state = store.getState().schema;
+    expect(state.status).toEqual(StoreStatus.loading);
+  });
+
+  it('should handle fetchSchema.rejected', async () => {
+    const mockError = 'Async error';
+    const fn = jest.fn().mockRejectedValue(new Error(mockError));
+    const mockClient = new SimpleMockApiClient<IntrospectionResponseData>(fn);
+    const initial = store.getState().schema;
+    expect(initial.status).toEqual(StoreStatus.idle);
+
+    await store.dispatch(fetchSchema({ client: mockClient, endpoint: mockEndpoint }));
+    const state = store.getState().schema;
+    expect(state.status).toEqual(StoreStatus.failed);
+    expect(state.endpoint).toBe(mockEndpoint);
+    expect(state.error).toBe(mockError);
+    expect(state.errorcounter).toBe(1);
+  });
+
+  it('should handle fetchSchema.fulfilled', async () => {
+    const mockClient = await setupMockIntrospection();
+    await store.dispatch(fetchSchema({ client: mockClient, endpoint: mockEndpoint }));
+
+    const state = store.getState().schema;
+    expect(state.status).toEqual(StoreStatus.succeeded);
+    expect(fuzzball.ratio(state.schema, MOCK_QUERY_EXPECTED)).toBeGreaterThan(98);
+    expect(state.endpoint).toEqual(mockEndpoint);
+    expect(state.error).toBeUndefined();
+    expect(state.errorcounter).toEqual(0);
+  });
+});
