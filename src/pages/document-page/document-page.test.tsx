66be8ecfef1fa7e34bc447bffd4bebde5e3bec3e
Valts Silaputnins <valts@velns.org>
2023-05-04 13:36:19 +0300

    feat: test case for error loading

--- src/pages/document-page/document-page.test.tsx
+++ src/pages/document-page/document-page.test.tsx
@@ -1,10 +1,13 @@
+import { SimpleMockApiClient } from '@/../__mocks__/SimpleMockApiClient';
 import { setupMockIntrospection } from '@/../__mocks__/api-mock-helper';
+import { fetchSchema } from '@/slices';
 import { setupStore } from '@/store';
 import { act, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { DocumentPageComponent } from './document-page';
-import { fetchSchema } from '@/slices';
+import { IntrospectionResponseData } from '@/core/api/api';
+import { waitRender } from '@/../__mocks__/test-utils';
 
 describe('Document page component', () => {
   it('should display root page and back button', async () => {
@@ -34,6 +37,24 @@ describe('Document page component', () => {
     act(() => back.click());
     expect(screen.getByRole('button', { name: 'Back' })).toBeDisabled();
     expect(screen.getByText('Queries')).toBeVisible();
+  });
+
+  it('should handle errors', async () => {
+    const mockError = 'Async error';
+    const fn = jest.fn().mockRejectedValue(new Error(mockError));
+    const mockClient = new SimpleMockApiClient<IntrospectionResponseData>(fn);
+    const store = setupStore();
+    await store.dispatch(fetchSchema({ client: mockClient, endpoint: 'dummy' }));
+
+    render(
+      <Provider store={store}>
+        <DocumentPageComponent />
+      </Provider>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText(mockError)).toBeInTheDocument();
+    });
   });
 
   async function defaultRender() {
