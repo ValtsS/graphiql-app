8186bd5ae6cba1662b145db37e547ad0c2018cbc
Evgen <yevgenchikk@mail.ru>
2023-05-05 15:49:30 +0300

    feat: add mock for monaco-editor

---
+++ __mocks__/monaco-editor.js
@@ -0,0 +1,11 @@
+module.exports = {
+  languages: {
+    json: {
+      jsonDefaults: {
+        setDiagnosticsOptions: function ({}) {},
+      },
+    },
+  },
+  KeyMod: { CtrlCmd: '' },
+  KeyCode: { Enter: '' },
+};
