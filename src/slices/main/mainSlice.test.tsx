dbaa7fa414f2771101d9e819b261efcca32a2fa5
Valts Silaputnins <valts@velns.org>
2023-05-09 12:35:11 +0300

    feat: add test for mainslice

---
+++ src/slices/main/mainSlice.test.tsx
@@ -0,0 +1,23 @@
+import { AppStore, setupStore } from '@/store';
+import { changeEndpoint } from './mainSlice';
+
+describe('schemaSlice', () => {
+  let store: AppStore;
+
+  beforeEach(() => {
+    store = setupStore();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should handle endpoint update', async () => {
+    const testString = 'dummy';
+    const initial = store.getState().main;
+    expect(initial.endpoint).not.toBe(testString);
+    store.dispatch(changeEndpoint({ endpoint: testString }));
+    const updated = store.getState().main;
+    expect(updated.endpoint).toBe(testString);
+  });
+});
