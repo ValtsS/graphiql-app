3ddaf357af1cb0ad53dd45988055e5b7e9043a10
Valts Silaputnins <valts@velns.org>
2023-05-17 23:21:26 +0300

    feat: test send query function

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -11,6 +11,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Main } from './Main';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('monaco-editor');
 
@@ -24,7 +25,7 @@ describe('Main page component', () => {
   });
 
   it('should render and have change button', async () => {
-    const store = await defaultRender();
+    const { store } = await defaultRender();
     const state = store.getState();
 
     expect(state.main.endpoint.toLowerCase().startsWith('http')).toBe(true);
@@ -35,7 +36,7 @@ describe('Main page component', () => {
   });
 
   it('should render Be able to change endpoint', async () => {
-    const store = await defaultRender();
+    const { store } = await defaultRender();
 
     const changeButton = screen.getByRole('button', { name: 'Change Endpoint' });
     expect(changeButton).toBeVisible();
@@ -57,9 +58,27 @@ describe('Main page component', () => {
     expect(screen.getByText(state.main.endpoint));
   });
 
+  it('should render and allow query to be pressed', async () => {
+    const { store, callback } = await defaultRender();
+    const state = store.getState();
+
+    expect(state.main.endpoint.toLowerCase().startsWith('http')).toBe(true);
+    expect(screen.getByText(state.main.endpoint));
+
+    const responseText = 'ZZZZDummy';
+    callback.mockResolvedValue(responseText);
+
+    const sendButton = screen.getByRole('button', { name: 'Send query' });
+    expect(sendButton).toBeVisible();
+    expect(sendButton).toBeEnabled();
+    await userEvent.click(sendButton);
+    await waitRender();
+    expect(store.getState().editors.response).toBe(`"${responseText}"`);
+  });
+
   async function defaultRender() {
     const store = setupStore();
-    const { mockClient } = await setupMockIntrospection();
+    const { mockClient, callback } = await setupMockIntrospection();
 
     act(() => {
       render(
@@ -77,6 +96,6 @@ describe('Main page component', () => {
       );
     });
     await waitRender();
-    return store;
+    return { store, callback };
   }
 });
