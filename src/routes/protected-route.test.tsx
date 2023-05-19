ca172d19ba7feb239354bcc678c2c6e8d1df6ac7
Valts Silaputnins <valts@velns.org>
2023-05-20 01:30:58 +0300

    feat: correct tests for protected route

--- src/routes/protected-route.test.tsx
+++ src/routes/protected-route.test.tsx
@@ -1,16 +1,54 @@
+import { FirebaseMock, SetupFirebaseMock } from '@/../__mocks__/firebaseMock';
+import { waitRender } from '@/../__mocks__/test-utils';
+import { AppContextProvider } from '@/provider';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { ProtectedRoute } from './protected-route';
-import { render } from '@testing-library/react';
 import { AccessMode } from './routes-config';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 
-test('renders ProtectedRoute component', () => {
-  const mockChildren = 'Test Child Component';
+describe('Protected Route', () => {
+  const mockChildrenText = 'Test Child Component';
+  const rootText = 'Redirected';
 
-  const { getByText } = render(
-    <ProtectedRoute mode={AccessMode.Always}>
-      <div>{mockChildren}</div>
-    </ProtectedRoute>
+  it.each([
+    [true, AccessMode.Always, true],
+    [false, AccessMode.Always, true],
+    [true, AccessMode.LoggedIn, true],
+    [false, AccessMode.LoggedIn, false],
+    [false, AccessMode.Guest, true],
+    [true, AccessMode.Guest, false],
+  ])(
+    'Test protected route loggedin=%s to be in mode=%s to be granted=%s',
+    async (loggedIn: boolean, mode: AccessMode, granted: boolean) => {
+      const auth = SetupFirebaseMock(loggedIn);
+      const { getByText, queryByText } = defaultRender(auth, mode);
+      if (granted) expect(getByText(mockChildrenText)).toBeInTheDocument();
+      else {
+        await waitRender();
+        expect(queryByText(mockChildrenText)).not.toBeInTheDocument();
+        expect(getByText(rootText)).toBeInTheDocument();
+      }
+    }
   );
 
-  expect(getByText(mockChildren)).toBeInTheDocument();
+  function defaultRender(auth: FirebaseMock, mode: AccessMode) {
+    return render(
+      <AppContextProvider apiClient={null} auth={auth}>
+        <MemoryRouter initialEntries={['/auth', '/']} initialIndex={0}>
+          <Routes>
+            <Route
+              path={'/auth'}
+              element={
+                <ProtectedRoute mode={mode}>
+                  <div>{mockChildrenText}</div>
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/" element={<>{rootText}</>}></Route>
+          </Routes>
+        </MemoryRouter>
+      </AppContextProvider>
+    );
+  }
 });
