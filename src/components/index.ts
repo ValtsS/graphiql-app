5afa5ca5d9dac15725ec13db35f0b1613635a79c
Valts Silaputnins <valts@velns.org>
2023-07-06 23:31:54 +0300

    refactor: remove

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './header';
 export * from './footer';
-export * from './sideBar';
 export * from './address-bar/address-bar';
 export * from './sdl-document-browser/sdl-document-browser';
 export * from './toaster/toaster';
